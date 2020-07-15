b686640301b6d142146b155aa5d5f6bfb4556bd8
roman.kson@gmail.com <roman.kson@gmail.com>
2020-07-15 12:10:09 +0300

    push

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,35 +1,13 @@
 import React, { Component } from "react";
 import generateWeekRange from "./generateWeekRange.js";
 
-class Navigation extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      week: new Date(this.props.date),
-    };
-  }
-
-  render() {
-    return (
-      <section className="date-container">
-        <div className="day-container timezone">GMT+02</div>
-        {generateWeekRange(this.state.week).map((el) => {
-          if (new Date(el).getDate() === new Date().getDate()) {
-            return (
-              <div className="day-container" key={el}>
-                <h1 className="day-container_text">
-                  {
-                    ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"][
-                      new Date(el).getDay()
-                    ]
-                  }
-                </h1>
-                <h2 className="day-container_number current-day">
-                  {new Date(el).getDate()}
-                </h2>
-              </div>
-            );
-          }
+const Navigation = ({ date }) => {
+  const week = generateWeekRange(date);
+  return (
+    <section className="date-container">
+      <div className="day-container timezone">GMT+02</div>
+      {week.map((el) => {
+        if (new Date(el).getDate() === new Date().getDate()) {
           return (
             <div className="day-container" key={el}>
               <h1 className="day-container_text">
@@ -39,12 +17,27 @@ class Navigation extends Component {
                   ]
                 }
               </h1>
-              <h2 className="day-container_number">{new Date(el).getDate()}</h2>
+              <h2 className="day-container_number current-day">
+                {new Date(el).getDate()}
+              </h2>
             </div>
           );
-        })}
-      </section>
-    );
-  }
-}
+        }
+        return (
+          <div className="day-container" key={el}>
+            <h1 className="day-container_text">
+              {
+                ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"][
+                  new Date(el).getDay()
+                ]
+              }
+            </h1>
+            <h2 className="day-container_number">{new Date(el).getDate()}</h2>
+          </div>
+        );
+      })}
+    </section>
+  );
+};
+
 export default Navigation;
